399b3cf8f33a47132c4574395282141105c4f515
adheesha0316 <adheeshaanuradha2001@gmail.com>
2025-12-23 08:36:29 +0530

    create UserService methods

--- src/main/java/com/spareparts/spareparts_backend/service/UserService.java
+++ src/main/java/com/spareparts/spareparts_backend/service/UserService.java
@@ -1,4 +1,25 @@
 package com.spareparts.spareparts_backend.service;
 
+import com.spareparts.spareparts_backend.entity.User;
+import com.spareparts.spareparts_backend.enums.Role;
+
+import java.util.List;
+import java.util.Optional;
+
 public interface UserService {
+    User registerUser(User user); // Register partner, manager, customer
+
+    User loginUser(String email, String password); // Basic login
+
+    Optional<User> getUserById(Integer id);
+
+    List<User> getAllUsers();
+
+    // Admin approves or disapproves a user (partner or manager)
+    User approveUser(Integer userId);
+
+    User disapproveUser(Integer userId);
+
+    // Change role (only admin)
+    User changeUserRole(Integer userId, Role role);
 }
