ce0b289f3320bd9804fd5700c8216a82aad7d9e0
adheesha0316 <adheeshaanuradha2001@gmail.com>
2025-12-23 05:46:26 +0530

    Initial Spring Boot setup

---
+++ src/main/java/com/spareparts/spareparts_backend/SparepartsBackendApplication.java
@@ -0,0 +1,13 @@
+package com.spareparts.spareparts_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SparepartsBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SparepartsBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/spareparts/spareparts_backend/SparepartsBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spareparts.spareparts_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SparepartsBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
