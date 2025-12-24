ad8a195fb2027dbdb5645066574bc11b6bf5c286
adheesha0316 <adheeshaanuradha2001@gmail.com>
2025-12-25 05:21:24 +0530

    create ManagerStatus

---
+++ src/main/java/com/spareparts/spareparts_backend/enums/ManagerStatus.java
@@ -0,0 +1,9 @@
+package com.spareparts.spareparts_backend.enums;
+
+public enum ManagerStatus {
+    PENDING,          // waiting admin approval
+    APPROVED,         // active
+    UPDATE_PENDING,   // updated, waiting admin
+    REJECTED,
+    SUSPENDED
+}
