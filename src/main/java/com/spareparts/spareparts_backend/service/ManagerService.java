e79d99e31f819900c0715d4e9d16bca5a96abf8b
adheesha0316 <adheeshaanuradha2001@gmail.com>
2025-12-25 06:19:01 +0530

    1st update ManagerService

--- src/main/java/com/spareparts/spareparts_backend/service/ManagerService.java
+++ src/main/java/com/spareparts/spareparts_backend/service/ManagerService.java
@@ -2,26 +2,35 @@ package com.spareparts.spareparts_backend.service;
 
 import com.spareparts.spareparts_backend.dto.ManagerDto;
 import org.springframework.core.io.Resource;
+import org.springframework.web.multipart.MultipartFile;
 
 import java.util.List;
 
 public interface ManagerService {
     // ---------------- CREATE ---------------- //
-    ManagerDto createManagerProfile(ManagerDto managerDto);
+    ManagerDto createManager(Integer userId, ManagerDto managerDto,
+                             MultipartFile nicFront, MultipartFile nicBack, MultipartFile profileImage);
+
 
     // ---------------- UPDATE ---------------- //
-    ManagerDto updateManagerProfile(Integer managerId, ManagerDto managerDto);
+    ManagerDto updateManager(Integer managerId, ManagerDto managerDto,
+                             MultipartFile nicFront, MultipartFile nicBack, MultipartFile profileImage);
+
 
     // ---------------- DELETE ---------------- //
     void deleteManagerProfile(Integer managerId);
 
+
     // ---------------- GET ---------------- //
     ManagerDto getManagerById(Integer managerId);
 
+
     List<ManagerDto> getAllManagers();
+
 
     // ---------------- ADMIN APPROVE ---------------- //
     ManagerDto approveManagerProfile(Integer managerId);
+
 
     // ---------------- NIC ZIP DOWNLOAD ---------------- //
     Resource downloadNICImagesAsZip(Integer managerId);
