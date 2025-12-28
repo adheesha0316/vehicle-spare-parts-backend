7859fb18a6a2fdadccc7e7a92e401126e541f373
adheesha0316 <adheeshaanuradha2001@gmail.com>
2025-12-29 03:35:47 +0530

    create AdminApprovalStatus

---
+++ src/main/java/com/spareparts/spareparts_backend/enums/AdminApprovalStatus.java
@@ -0,0 +1,7 @@
+package com.spareparts.spareparts_backend.enums;
+
+public enum AdminApprovalStatus {
+    PENDING,    // Waiting for admin approval
+    APPROVED,   // Approved by admin
+    REJECTED    // Rejected by admin
+}
