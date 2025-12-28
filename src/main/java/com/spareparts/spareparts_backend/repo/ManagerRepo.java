2f934a7261d35ddb9abf115cf1cd5b88546f2f41
adheesha0316 <adheeshaanuradha2001@gmail.com>
2025-12-29 01:07:20 +0530

    update ManagerRepo

--- src/main/java/com/spareparts/spareparts_backend/repo/ManagerRepo.java
+++ src/main/java/com/spareparts/spareparts_backend/repo/ManagerRepo.java
@@ -1,14 +1,22 @@
 package com.spareparts.spareparts_backend.repo;
 
 import com.spareparts.spareparts_backend.entity.Manager;
+import com.spareparts.spareparts_backend.enums.ManagerStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface ManagerRepo extends JpaRepository<Manager,Integer> {
-    // Find manager by linked user
-    Optional<Manager> findByUserUserId(Integer userId);
+    // 🔹 Find manager by linked user (exclude deleted)
+    Optional<Manager> findByUserUserIdAndStatusNot(Integer userId, ManagerStatus status);
+
+    // 🔹 Get all non-deleted managers
+    List<Manager> findByStatusNot(ManagerStatus status);
+
+    // 🔹 Get manager by id (exclude deleted)
+    Optional<Manager> findByManagerIdAndStatusNot(Integer managerId, ManagerStatus status);
 
 }
