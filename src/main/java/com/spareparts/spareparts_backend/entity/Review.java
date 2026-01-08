b45823f82d275e076ac226f0f4623d50350e9262
adheesha0316 <adheeshaanuradha2001@gmail.com>
2026-01-08 11:55:31 +0530

    create Review entity

---
+++ src/main/java/com/spareparts/spareparts_backend/entity/Review.java
@@ -0,0 +1,36 @@
+package com.spareparts.spareparts_backend.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "reviews")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Review {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer reviewId;
+
+    @ManyToOne
+    @JoinColumn(name = "customer_id", nullable = false)
+    private Customer customer;
+
+    @ManyToOne
+    @JoinColumn(name = "spare_item_id", nullable = false)
+    private SpareItem spareItem;
+
+    private int rating;
+    private String comment;
+
+    @CreationTimestamp
+    private LocalDateTime createdAt;
+}
