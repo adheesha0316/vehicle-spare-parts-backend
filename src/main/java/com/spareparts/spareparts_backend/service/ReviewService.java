e826bf20ab028308238d7495b65a6f32275f02e8
adheesha0316 <adheeshaanuradha2001@gmail.com>
2026-01-08 15:18:20 +0530

    create ReviewService

---
+++ src/main/java/com/spareparts/spareparts_backend/service/ReviewService.java
@@ -0,0 +1,25 @@
+package com.spareparts.spareparts_backend.service;
+
+import com.spareparts.spareparts_backend.dto.ReviewRequestDto;
+import com.spareparts.spareparts_backend.dto.ReviewResponseDto;
+
+import java.util.List;
+
+public interface ReviewService {
+    ReviewResponseDto addReview(
+            Integer customerId,
+            ReviewRequestDto requestDto
+    );
+
+    List<ReviewResponseDto> getReviewsBySpareItem(Integer spareItemId);
+
+    List<ReviewResponseDto> getCustomerReviews(Integer customerId);
+
+    void deleteReview(
+            Integer customerId,
+            Integer reviewId
+    );
+
+    // ADMIN override
+    void deleteReviewByAdmin(Integer reviewId);
+}
