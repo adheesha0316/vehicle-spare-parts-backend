9e1d18d04a25b92ea70277f09b539a2510f66ab2
adheesha0316 <adheeshaanuradha2001@gmail.com>
2026-01-08 11:44:38 +0530

    create Cart and CartItem entities

---
+++ src/main/java/com/spareparts/spareparts_backend/entity/Cart.java
@@ -0,0 +1,27 @@
+package com.spareparts.spareparts_backend.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "carts")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Cart {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer cartId;
+
+    @OneToOne
+    @JoinColumn(name = "customer_id", nullable = false)
+    private Customer customer;
+
+    @OneToMany(mappedBy = "cart", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<CartItem> items;
+}
---
+++ src/main/java/com/spareparts/spareparts_backend/entity/CartItem.java
@@ -0,0 +1,29 @@
+package com.spareparts.spareparts_backend.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "cart_items")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class CartItem {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @ManyToOne
+    @JoinColumn(name = "cart_id", nullable = false)
+    private Cart cart;
+
+    @ManyToOne
+    @JoinColumn(name = "spare_item_id", nullable = false)
+    private SpareItem spareItem;
+
+    private int quantity;
+}
