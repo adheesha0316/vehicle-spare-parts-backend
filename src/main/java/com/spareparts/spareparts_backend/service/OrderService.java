1b0a71667101d69d05fde582a5b013cdb3e14f4a
adheesha0316 <adheeshaanuradha2001@gmail.com>
2026-01-08 15:15:40 +0530

    create OrderService

---
+++ src/main/java/com/spareparts/spareparts_backend/service/OrderService.java
@@ -0,0 +1,32 @@
+package com.spareparts.spareparts_backend.service;
+
+import com.spareparts.spareparts_backend.dto.OrderResponseDto;
+import com.spareparts.spareparts_backend.enums.OrderStatus;
+
+import java.util.List;
+
+public interface OrderService {
+    // ================= PLACE ORDER =================
+    OrderResponseDto placeOrder(Integer customerId);
+
+    // ================= CUSTOMER =================
+    List<OrderResponseDto> getOrderHistory(Integer customerId);
+
+    OrderResponseDto getOrderDetails(Integer orderId);
+
+    void cancelOrder(
+            Integer customerId,
+            Integer orderId
+    );
+
+    // ================= ADMIN / PARTNER =================
+    void updateOrderStatus(
+            Integer orderId,
+            OrderStatus status
+    );
+
+    List<OrderResponseDto> getAllOrders();
+
+    // ================= INTERNAL =================
+    boolean canCancelOrder(Integer orderId);
+}
