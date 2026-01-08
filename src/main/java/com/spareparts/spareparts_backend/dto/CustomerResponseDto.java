b24de2d6d680f4a7667525f2b1273206c429323f
adheesha0316 <adheeshaanuradha2001@gmail.com>
2026-01-08 12:29:52 +0530

    update CustomerResponseDto

--- src/main/java/com/spareparts/spareparts_backend/dto/CustomerResponseDto.java
+++ src/main/java/com/spareparts/spareparts_backend/dto/CustomerResponseDto.java
@@ -14,6 +14,7 @@ import java.time.LocalDateTime;
 @Builder
 public class CustomerResponseDto {
     private Integer customerId;
+    private Integer userId;
 
     private String fullName;
     private String nicNumber;
@@ -22,7 +23,6 @@ public class CustomerResponseDto {
 
     private String profileImage;
 
-    private CustomerStatus status;
-
     private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
 }
