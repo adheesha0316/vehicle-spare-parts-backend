4464855357c6a066e577fb3fa0a99649062a0775
adheesha0316 <adheeshaanuradha2001@gmail.com>
2026-01-08 12:02:54 +0530

    create Wallet entity

---
+++ src/main/java/com/spareparts/spareparts_backend/entity/Wallet.java
@@ -0,0 +1,27 @@
+package com.spareparts.spareparts_backend.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+
+@Entity
+@Table(name = "wallets")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Wallet {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer walletId;
+
+    @OneToOne
+    @JoinColumn(name = "customer_id", nullable = false)
+    private Customer customer;
+
+    private BigDecimal balance;
+}
